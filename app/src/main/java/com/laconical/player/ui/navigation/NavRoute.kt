b29aaab21b0d157e13294c638eb8819df687ebca
Btema2 <temabiill@gmail.com>
2026-04-18 21:41:29 +0200

    feat: add NavRoute definitions and stub screens for Albums/Artists/Playlists

---
+++ app/src/main/java/com/laconical/player/ui/navigation/NavRoute.kt
@@ -0,0 +1,8 @@
+package com.laconical.player.ui.navigation
+
+object NavRoute {
+    const val TRACKS = "tracks"
+    const val ALBUMS = "albums"
+    const val ARTISTS = "artists"
+    const val PLAYLISTS = "playlists"
+}
