cf1b5dea8d227102357ddac16e88ecba49202988
Btema2 <temabiill@gmail.com>
2026-06-17 21:57:57 +0200

    fix(deps): remove unused foojay toolchain resolver plugin
    The foojay-resolver-convention plugin (1.0.0) was added by default but
    no java {} toolchain block is declared anywhere in the project. It was
    the source of all Netty, jose4j, and commons-lang3 transitive
    vulnerabilities tracked by Dependabot (#10-#15, #17, #19-#20, #24-#32,
    #35-#39). Gradle 9.5.1 resolves toolchains natively without this plugin.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,26 +5,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-buildscript {
-    dependencies {
-        constraints {
-            // Force patched versions of vulnerable transitive deps pulled in by build plugins.
-            add("classpath", "io.netty:netty-codec:4.2.15.Final")
-            add("classpath", "io.netty:netty-codec-http:4.2.15.Final")
-            add("classpath", "io.netty:netty-codec-http2:4.2.15.Final")
-            add("classpath", "io.netty:netty-common:4.2.15.Final")
-            add("classpath", "io.netty:netty-handler:4.2.15.Final")
-            add("classpath", "io.netty:netty-handler-proxy:4.2.15.Final")
-            add("classpath", "org.bouncycastle:bcprov-jdk18on:1.84")
-            add("classpath", "org.bouncycastle:bcpkix-jdk18on:1.84")
-            add("classpath", "org.bitbucket.b_c:jose4j:0.9.6")
-            add("classpath", "org.apache.commons:commons-lang3:3.18.0")
-        }
-    }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
